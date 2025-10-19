3f3fd02797d5ed5681f913d2fadc515b08aab526
skanmacos <skanmacos@gmail.com>
2025-10-19 17:30:09 +0000

    skanherov1

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -1,6 +1,7 @@
-# To learn more about how to use Nix to configure your environment
-# see: https://firebase.google.com/docs/studio/customize-workspace
-{ pkgs, ... }: {
+# To learn more about how to use Nix to configure your environment:
+# https://firebase.google.com/docs/studio/customize-workspace
+
+{ _pkgs, ... }: {
   # Which nixpkgs channel to use.
   channel = "stable-24.05"; # or "unstable"
 
